d6aba23213ce0ac9544aab51a0ebbaee984b793c
lee-hyunsu <dlgustn675@naver.com>
2021-02-02 17:40:39 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Typography} from 'antd';
-import Chatbot from './Chatbot/Chatbot'
+import Chatbot from './Chatbot/Chatbot';
+import {AiFillLike} from "react-icons/ai";
 
 const {Title} = Typography;
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div>
       <div style={{display: 'flex', justifyContent: 'center', marginTop: '2rem'}}>
-        <Title level={2} >ChatBot Web&nbsp;</Title>
+        <Title level={2} >ChatBot Web&nbsp;<AiFillLike /></Title>
       </div>
       <div style={{display: 'flex', justifyContent: 'center'}}>
         <Chatbot />
--- src/Chatbot/Chatbot.js
+++ src/Chatbot/Chatbot.js
@@ -4,16 +4,16 @@ function Chatbot() {
     return (
         <div style={{height: 700, width: 700,
                      border: '3px solid black', borderRadius: '7px'}}>
-            대화 내용을 입력해주세요...?
         
-        <div style={{height: 623, width: '100%', overflow: 'auto'}}>
-
+        <div style={{height: 644, width: '100%', overflow: 'auto', 
+                     backgroundColor: 'white', borderRadius: '4px'}}>
+            하이
         </div>
 
         <input style={{
             margin: 0, width: '100%', height: 50,
             borderRadius: '4px', padding: '5px', fontSize: '1rem'
-        }} />
+        }} placeholder="챗봇에게 메시지를 보내보세요." type="text"/>
 
         </div>
     )
