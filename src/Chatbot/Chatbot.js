1b8b2491163d5fdf462f6419d3ead47820560f67
lee-hyunsu <dlgustn675@naver.com>
2021-02-02 16:21:26 +0900

    test

---
+++ src/Chatbot/Chatbot.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Chatbot() {
+    return (
+        <div style={{height: 700, width: 700,
+                     border: '3px solid black', borderRadius: '7px'}}>
+            Chatbot
+        </div>
+    )
+}
+
+export default Chatbot;
